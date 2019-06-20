3b0396c6b041cdd1bd6f9152d7b16dd50c333e1a
Oliwia Brożek <oliwiabrozek@gmail.com>
2019-06-20 10:49:47 +0200

    okno logowania singleton

--- PhotoManager/PhotoManager/Program.cs
+++ PhotoManager/PhotoManager/Program.cs
@@ -14,9 +14,12 @@ namespace PhotoManager
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+			
+			Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+			
+			Application.Run(LoggingWindow.getInstance());
+            //Application.Run(new Form1());
         }
     }
 }
